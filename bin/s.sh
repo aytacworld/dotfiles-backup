37397dbd136c4a0733a4871dd2fdf5bd9932ce2f
Adem Aytac <adem.aytac@flowpilots.com>
2020-10-07 11:06:26 +0200

    editorconfig added & git script updated

--- bin/s.sh
+++ bin/s.sh
@@ -11,37 +11,43 @@ foldername="$tempFolderName/$(basename $(pwd))"
 previousBranch=$([[ ! -d $tempFolderName ]] && mkdir $tempFolderName; [[ ! -f "$foldername" ]] && touch "$foldername" && echo $branch > "$foldername" || cat "$foldername")
 
 checkout () {
-    echo $branch > $foldername
-    git checkout $1
+  echo $branch > $foldername
+  git checkout $1
 }
 
 create () {
-    git checkout -b $1
+  git checkout -b $1
 }
 
 commit () {
-    git add .
-    git commit -m "$1"
+  git add .
+  git commit -m "$1"
 }
 
 case $1 in
-    commit)
-        commit "$2"
-        ;;
-    pull)
-        git pull
-        ;;
-    push)
-        [[ ! -z "$2" ]] && commit "$2"
-        git push -u $remote $branch
-        ;;
-    create)
-        create $2
-        ;;
-    back)
-        checkout $previousBranch
-        ;;
-    *)
-        [ -z "$1" ] && git status || checkout $1
-        ;;
+  commit)
+    commit "$2"
+    ;;
+  pull)
+    git pull
+    ;;
+  push)
+    [[ ! -z "$2" ]] && commit "$2"
+    git push -u $remote $branch
+    ;;
+  force)
+    git push -f
+    ;;
+  create)
+    create $2
+    ;;
+  back)
+    checkout $previousBranch
+    ;;
+  squash)
+    git rebase -i HEAD~$2
+    ;;
+  *)
+    [ -z "$1" ] && git status || checkout $1
+    ;;
 esac
