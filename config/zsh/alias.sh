4e2adcc9d948aae47979a68a4149ce4640bb8130
Adem Aytac <adem@aytacworld.com>
2022-05-18 12:48:08 +0200

    docker aliases

--- config/zsh/alias.sh
+++ config/zsh/alias.sh
@@ -2,6 +2,6 @@ alias v="vim -N"
 alias s="$BIN/s.sh"
 alias q="exit"
 alias d-rm="docker ps -q -a | xargs docker stop && docker ps -q -a | xargs docker rm"
-alias d-rmi="docker rmi $(docker images -a -q)"
+alias d-rmi="docker images -a -q | xargs docker rmi"
 
 source $DOTFILES/private/zsh/alias.sh
