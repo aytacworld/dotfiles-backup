84d11646541bca53ee9f9cb795a333aa174d1e4e
Adem Aytac <adem@aytacworld.com>
2022-12-23 19:36:01 +0100

    aliases updated

--- config/zsh/alias.sh
+++ config/zsh/alias.sh
@@ -3,5 +3,6 @@ alias s="$BIN/s.sh"
 alias q="exit"
 alias d-rm="docker ps -q -a | xargs docker stop && docker ps -q -a | xargs docker rm"
 alias d-rmi="docker images -a -q | xargs docker rmi"
+alias t="gnome-terminal ."
 
 source $DOTFILES/private/zsh/alias.sh
